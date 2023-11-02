b3458c57e0a282723f9e74c343b45000aa589af0
27rajyash <149619502+27rajyash@users.noreply.github.com>
2023-11-02 16:18:30 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    int sub, mul, div;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+    sub = num1 - num2;
+    mul = num1 * num2;
+    div = num1 / num2;
+
+    printf("Sum of the numbers: %d\n", sum);
+    printf("Subtraction of the numbers: %d\n", sub);
+    printf("Multiplication of the numbers: %d\n", mul);
+    printf("Division of the numbers: %d\n", div);
+
+    return 0;
+}
